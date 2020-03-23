e2e54b99328a147e35fc6514dadaeb8c24dc757a
Justin Abrahms <justin@abrah.ms>
2020-03-23 16:12:52 -0700

    readme should be the index file

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-mkdir _site/ || true
+mkdir -p _site/
+
 for POST in *.md; do
-    pandoc --to=html5 --css https://cdn.rawgit.com/yegor256/tacit/gh-pages/tacit-css-1.3.1.min.css --output=_site/$(basename ${POST%.md}).html --standalone $POST
+    if [ $POST == 'README.md' ]; then
+        OUTPOST=index.html
+    else
+        OUTPOST=${POST%.md}.html
+    fi
+    pandoc --to=html5 --css https://cdn.rawgit.com/yegor256/tacit/gh-pages/tacit-css-1.3.1.min.css --output=_site/$(basename $OUTPOST) --standalone $POST
 done
